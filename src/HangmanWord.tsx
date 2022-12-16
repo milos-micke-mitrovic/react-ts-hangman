d73339ec26900a4576ed5a57a87515dbac867846
mmitrovic <Milos.Mitrovic@rt-rk.com>
2022-12-16 15:57:23 +0100

    First commit

---
+++ src/HangmanWord.tsx
@@ -0,0 +1,43 @@
+type HangmanWordProps = {
+  wordToGuess: string;
+  guessedLetters: string[];
+  reveal?: boolean;
+};
+
+const HangmanWord = ({
+  guessedLetters,
+  wordToGuess,
+  reveal = false,
+}: HangmanWordProps) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {wordToGuess.split("").map((letter, index) => (
+        <span key={index} style={{ borderBottom: ".1em solid black" }}>
+          <span
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) && reveal ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+};
+
+export default HangmanWord;
